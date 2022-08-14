1f0bee2ec79aad708bbd0a2edd3036052cccece7
Nicolas Rodrigues <nicolasdourado04@gmail.com>
2022-08-13 22:47:36 -0400

    commit

---
+++ bee1001.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+    int a;
+    int b;
+    int x;
+
+    scanf("%i", &a);
+    scanf("%i", &b);
+
+    x = a + b;
+
+    printf("X = %i\n", x);
+
+    return 0;
+}
---
+++ bee1002.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    double pi = 3.14159;
+    double raio;
+    double area;
+
+    scanf("%lf", &raio);
+
+    area = pi * (raio* raio);
+
+    printf("A=%.4f\n", area);
+
+    return 0;
+}
